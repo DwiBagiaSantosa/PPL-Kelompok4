fdba577fb0655ec8109265909460772527175ce5
Dwi Bagia Santosa <dwibagiasentosa@gmail.com>
2023-05-14 20:36:32 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+// const session = require('express-session')
+
+const app = express()
+
+app.use(express.static('public'))
+
+//set view engine to ejs
+app.set('view engine', 'ejs');
+
+
+app.get('/',(req,res)=> {
+    res.render('admin/login');
+})
+
+
+
+
+const port = 3000;
+app.listen(port, () => {
+    console.log("Server started at port "+port)
+    console.log("http://localhost:"+port)
+})
