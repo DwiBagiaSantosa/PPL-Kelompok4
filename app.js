e0a265217a0b05112b93ea416a8240501544b45f
Dwi Bagia Santosa <dwibagiasentosa@gmail.com>
2023-06-06 21:26:48 +0700

    nyambungin ke database

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
 const express = require('express')
-// const session = require('express-session')
+const mysql = require('mysql');
+const session = require('express-session')
 
-const app = express()
+const port = 3001;
+
+const connection = mysql.createConnection({
+	host     : 'localhost',
+	user     : 'root',
+	password : '',
+	database : 'fabbis'
+});
+
+connection.connect(function(err) {
+	if (err) {
+	  console.log("Cannot connect to mysql...")
+	  throw err
+	}
+	console.log('Connected to mysql...')
+});
+
+const app = express();
+app.use(session({secret: 'secret',}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'))
 
@@ -13,6 +34,30 @@ app.get('/',(req,res)=> {
     res.render('admin/login');
 })
 
+app.post('/auth-mhs', (req, res) => {
+    let username = req.body.username;
+    let password = req.body.password;
+
+    if (username && password) {
+        connection.query('SELECT * FROM users WHERE username = ? AND password = ?', [username, password], (err, results, fields) => {
+            if (err) throw err;
+
+            if (results.length > 0) {
+                req.session.loggedin = true;
+                req.session.username = username;
+
+                res.redirect('/user');
+            } else {
+                res.redirect('/')
+            }
+            res.end();
+        });
+    } else {
+        res.send('Invalid')
+        res.end();
+    }
+})
+
 app.get('/dashboard',(req,res)=> {
     res.render('admin/dashboard_admin');
 })
@@ -22,7 +67,17 @@ app.get('/user',(req,res)=> {
 })
 
 app.get('/items',(req,res)=> {
-    res.render('user/items');
+    if(req.session.loggedin) {
+        connection.query('SELECT * FROM items', (err, results) => {
+            if (err) throw err;
+            res.render('user/items', {
+                dtd: results
+            });
+        })
+    } else {
+        res.redirect('/')
+    }
+    
 })
 
 app.get('/transaction',(req,res)=> {
@@ -34,13 +89,14 @@ app.get('/history',(req,res)=> {
 })
 
 app.get('/logout',(req,res)=> {
+    req.session.destroy();
 	res.redirect('/');
 })
 
 
 
 
-const port = 3001;
+
 app.listen(port, () => {
     console.log("Server started at port "+port)
     console.log("http://localhost:"+port)
---
+++ database/fabbis.sql
@@ -0,0 +1,98 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 06, 2023 at 04:24 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `fabbis`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `items`
+--
+
+CREATE TABLE `items` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `quantity` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `items`
+--
+
+INSERT INTO `items` (`id`, `nama`, `quantity`) VALUES
+(1, 'Ball', 4),
+(2, 'Jersey', 20);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`) VALUES
+(1, '1207050030', 'test123');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `items`
+--
+ALTER TABLE `items`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `items`
+--
+ALTER TABLE `items`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
